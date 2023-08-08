1f894e17a19812f105ee2a88161f5beccf7f8d4c
josetann <joseph@thetannerfamily.com>
2023-08-08 10:41:43 -0500

    Remove Registration Error

--- selfdrive/athena/registration.py
+++ selfdrive/athena/registration.py
@@ -66,34 +66,34 @@ def register(show_spinner=False) -> Optional[str]:
 
     backoff = 0
     start_time = time.monotonic()
-    while True:
-      try:
-        register_token = jwt.encode({'register': True, 'exp': datetime.utcnow() + timedelta(hours=1)}, private_key, algorithm='RS256')
-        cloudlog.info("getting pilotauth")
-        resp = api_get("v2/pilotauth/", method='POST', timeout=15,
-                       imei=imei1, imei2=imei2, serial=serial, public_key=public_key, register_token=register_token)
-
-        if resp.status_code in (402, 403):
-          cloudlog.info(f"Unable to register device, got {resp.status_code}")
-          dongle_id = UNREGISTERED_DONGLE_ID
-        else:
-          dongleauth = json.loads(resp.text)
-          dongle_id = dongleauth["dongle_id"]
-        break
-      except Exception:
-        cloudlog.exception("failed to authenticate")
-        backoff = min(backoff + 1, 15)
-        time.sleep(backoff)
-
-      if time.monotonic() - start_time > 60 and show_spinner:
-        spinner.update(f"registering device - serial: {serial}, IMEI: ({imei1}, {imei2})")
-
-    if show_spinner:
-      spinner.close()
+#    while True:
+#      try:
+#        register_token = jwt.encode({'register': True, 'exp': datetime.utcnow() + timedelta(hours=1)}, private_key, algorithm='RS256')
+#        cloudlog.info("getting pilotauth")
+#        resp = api_get("v2/pilotauth/", method='POST', timeout=15,
+#                       imei=imei1, imei2=imei2, serial=serial, public_key=public_key, register_token=register_token)
+#
+#        if resp.status_code in (402, 403):
+#          cloudlog.info(f"Unable to register device, got {resp.status_code}")
+#          dongle_id = UNREGISTERED_DONGLE_ID
+#        else:
+#          dongleauth = json.loads(resp.text)
+#          dongle_id = dongleauth["dongle_id"]
+#        break
+#      except Exception:
+#        cloudlog.exception("failed to authenticate")
+#        backoff = min(backoff + 1, 15)
+#        time.sleep(backoff)
+#
+#      if time.monotonic() - start_time > 60 and show_spinner:
+#        spinner.update(f"registering device - serial: {serial}, IMEI: ({imei1}, {imei2})")
+#
+#    if show_spinner:
+#      spinner.close()
 
   if dongle_id:
     params.put("DongleId", dongle_id)
-    set_offroad_alert("Offroad_UnofficialHardware", (dongle_id == UNREGISTERED_DONGLE_ID) and not PC)
+#    set_offroad_alert("Offroad_UnofficialHardware", (dongle_id == UNREGISTERED_DONGLE_ID) and not PC)
   return dongle_id
 
 
